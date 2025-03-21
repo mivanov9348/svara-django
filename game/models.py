a5a501a44d49be8bdce32aaa155382a1c17def2f
mivanov9348 <mivanov93@abv.bg>
2025-03-21 09:39:49 +0200

    initial commit

---
+++ game/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+class Card(models.Model):
+    SUITS = [('♠', 'Spades'), ('♥', 'Hearts'), ('♦', 'Diamonds'), ('♣', 'Clubs')]
+    RANKS = [('7', 7), ('8', 8), ('9', 9), ('10', 10), ('J', 10), ('Q', 10), ('K', 10), ('A', 11)]
+
+    rank = models.CharField(max_length=2, choices=[(r[0], r[0]) for r in RANKS])
+    suit = models.CharField(max_length=1, choices=SUITS)
+
+    def __str__(self):
+        return f"{self.rank}{self.suit}"
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=50)
+    chips = models.IntegerField(default=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    players = models.ManyToManyField(Player, through='PlayerHand')
+    pot = models.IntegerField(default=0)
+    dealer = models.ForeignKey(Player, on_delete=models.SET_NULL, null=True, related_name='dealt_games')
+
+class PlayerHand(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    cards = models.ManyToManyField(Card)
+    bet = models.IntegerField(default=0)
